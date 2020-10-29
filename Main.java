5b19d61448095a8c484906502fb9feeb9afbc024
qurek9038 <qurek9038@wrdsb.ca>
2020-10-29 20:16:42 +0000

    Completed For Loop Test

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will ask the user for the size of a line. It will then print to the screen the alternating text "black" and "White" the number of times that the user entered
+ * @author Khazina Qureshi
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //Create the scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask the user to enter the length of the line
+    System.out.println("Please enter the length of the line:");
+
+    //Store userinput as variable
+    int length = input.nextInt();
+
+    //Create the for loop to loop the users number through the modulus operation 
+    for (int i = 1; i <= length; i++){
+      if (i%2==0){
+        //if odd print white
+        i = i;
+        System.out.print("white,");
+      }
+        //else print black 
+      else { i = i;
+        System.out.print("black,");}
+    }
   }
 }
